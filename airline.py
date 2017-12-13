ffe6c8eabc86daea4362cc92c2d29f731549da0d
hugo-valle <hugovalle1@weber.edu>
2017-12-13 15:12:49 -0700

    Include make_boarding_cards to Aircraft class

--- airline.py
+++ airline.py
@@ -104,7 +104,19 @@ class Flight:
         return sum(sum(1 for s in row.values() if s is None)
                 for row in self._seating if row is not None)
 
+    def make_boarding_cards(self, card_printer):
+        for passenger, seat in sorted(self._passenger_seats()):
+            card_printer(passenger, seat, self.number(),
+                         self.aircraft_model())
 
+    def _passenger_seats(self):
+        """An iterable series of passengers seating allocations"""
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for letter in seat_letters:
+                passenger = self._seating[row][letter]
+                if passenger is not None:
+                    yield (passenger, "{}{}".format(row, letter))
 
 class Aircraft:
 
@@ -148,12 +160,30 @@ def make_flights():
     return f
 
 
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = "| Name: {0}"\
+             "  Flight: {1}"\
+             "  Seat: {2}"\
+             "  Aircraft: {3}"\
+            " |".format(passenger, flight_number, seat, aircraft)
+    banner = "+" + "-"*(len(output) -2)+ "+"
+    border = "|" + " "*(len(output) -2) + "|"
+    lines = [banner, border, output, border, banner]
+    card = '\n'.join(lines)
+    print(card)
+    print()
+
+
 def main():
     f1 = make_flights()
-    pp(f1._seating)
+    #pp(f1._seating)
     f1.reallocate_pasengers("22E", "12C")
-    pp(f1._seating)
-    print(f1.num_available_seats())
+    #pp(f1._seating)
+    #print(f1.num_available_seats())
+    # Pass the function as a parameter. Do not include the
+    # (), otherwise Python will try to execute the function.
+    f1.make_boarding_cards(console_card_printer)
+
 
 
 
