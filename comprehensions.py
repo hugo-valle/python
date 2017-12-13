3e81a35f66efc6963cd45ee124863252f8ea14c7
hugo-valle <hugovalle1@weber.edu>
2017-12-13 09:37:47 -0700

    First commit

---
+++ comprehensions.py
@@ -0,0 +1,39 @@
+from math import sqrt
+
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+def first(iterable):
+    iterator = iter(iterable)
+    try:
+        return next(iterator)
+    except StopIteration:
+        return ValueError("Iterable is empty")
+
+
+def gen246():
+    print("About to yield 2")
+    yield 2
+    print("About to yield 4")
+    yield 4
+    print("About to yield 6")
+    yield 6
+    print("About to return")
+
+
+def main():
+    l = [x for x in range(100001) if is_prime(x)]
+    print(l)
+    print(len(l))
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
