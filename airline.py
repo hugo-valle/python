1883e3c97bd30d7188ca871ab5bdcc29da8b264f
hugo-valle <hugovalle1@weber.edu>
2017-12-13 13:26:57 -0700

    First commit

--- airline.py
+++ airline.py
@@ -1,7 +1,7 @@
 """
 Model for aircraft fligths
 """
-
+from pprint import pprint as pp
 
 class Flight:
 
@@ -18,6 +18,17 @@ class Flight:
         self._number = number
         self._aircraft = aircraft
 
+        rows, seats = self._aircraft.seating_plan()
+        # List of Dictionaries
+        # Waste one entry to match the actual number from the map
+        # To the single element list, concatenate another list containing
+        #   one entry for each row in the aircraft. Done by the list
+        #   comprehension from the list of rows retrieve from the seating plan
+        # Discard the row number (_) since you already have it from before
+        # The item form the comprehension is itself a dictionary comprehension
+        # Use the list comprehension because we want a distinct object for each row
+        self._seating = [None] + [{letter:None for letter in seats} for _ in rows]
+
     def number(self):
         return self._number
 
@@ -26,6 +37,33 @@ class Flight:
 
     def aircraft_model(self):
         return self._aircraft.model()
+
+    def allocate_seats(self, seat, passenger):
+        """
+        Allocate a seat to a passenger
+        :param seat: A seat designator such '12A', '21F'
+        :param passenger: The passenger name
+        :raises: ValueError: if the seat is unavailable
+        """
+        rows, seat_letter = self._aircraft.seating_plan()
+        # Validate letter
+        letter = seat[-1]
+        if letter not in seat_letter:
+            raise ValueError("Invalid seat letter{}".format(letter))
+        # validate row format
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row{}".format(row_text))
+        # Valid row?
+        if row not in rows:
+            raise ValueError("Invalid row number{}".format(row))
+        # Check if it is available
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} is already occupied".format(seat))
+        # Assign seat
+        self._seating[row][letter] = passenger
 
 
 class Aircraft:
@@ -57,19 +95,16 @@ class Aircraft:
 
 
 def main():
-    """
-    Test Function
-    """
-    # f = Flight("AA123")
-    # print(f.number())
-    # print(f.airline())
     a = Aircraft("G-EUPT", "AirBus A319", num_rows=22,
                   num_seats_per_row=6)
-    # print(a.model())
-    # print(a.registration())
-    # print(a.seating_plan())
     f = Flight("AA777", a)
-    print(f.aircraft_model())
+    f.allocate_seats("12A", "Guido van Rossum")     # python
+    f.allocate_seats("12B", "Rasmus Lerdorf")       # php
+    f.allocate_seats("15F", "Bjarne Stroustrup")    # c++
+    f.allocate_seats("15E", "Anders Hejlsberg")     # Turbo Pascal
+    #f.allocate_seats("E27", "Yukihiro Matsumoto")   # Ruby
+    f.allocate_seats("22E", "Yukihiro Matsumoto")   # Ruby
+    pp(f._seating)
 
 
 if __name__ == '__main__':
