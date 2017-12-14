238aee3310a4aed68f3af793c1c7219838789a8c
Hugo Valle <hugovalle1@weber.edu>
2017-12-14 09:31:40 -0700

    frist commit

--- fridge.py
+++ fridge.py
@@ -1,5 +1,25 @@
+from contextlib import closing
+
 class RefrigeratorRaider:
     """
     Raid a Refrigerator
     """
-    pass
+    def open(self):
+        print("open fridge door")
+
+    def take(self, food):
+        print("Finding {}..".format(food))
+        if food == 'deep fried pizza':
+            raise RuntimeError("Health warning!")
+        print("Taking {}".format(food))
+
+    def close(self):
+        print("Close fridge door")
+
+
+def raid(food):
+    #r = RefrigeratorRaider()
+    with closing(RefrigeratorRaider()) as r:
+        r.open()
+        r.take(food)
+        #r.close() # Do you not need this close
